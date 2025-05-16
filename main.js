80b2c2798fef99c47ac755bb540382f1e13b6e39
Rdataflow <waldgis@bafu.admin.ch>
2025-05-16 14:41:50 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,60 +1,143 @@
-import Map from "ol/Map.js";
-import TileLayer from "ol/layer/WebGLTile.js";
-import GeoTIFF from "ol/source/GeoTIFF.js";
+import Map from 'ol/Map.js';
+import View from 'ol/View.js';
+import TileLayer from 'ol/layer/WebGLTile.js';
+import GeoTIFF from 'ol/source/GeoTIFF.js';
 
-const source = new GeoTIFF({
-  sources: [
-    {
-      url: "https://windwurfs1.s3.eu-central-1.amazonaws.com/S1_LRW_PD12_SP6_20150107-20150118.tif",
-      bands: [1, 2],
-      min: -30.0,
-      nodata: 0,
-      max: 30.0,
-    },
-    {
-      url: "https://windwurfs1.s3.eu-central-1.amazonaws.com/S1_LRW_PD12_SP6_20150101-20150112.tif",
-      bands: [1, 2],
-      min: -30.0,
-      nodata: 0,
-      max: 30.0,
-    },
+const max = 3000;
+function normalize(value) {
+  return ['/', value, max];
+}
+
+const red = normalize(['band', 1]);
+const green = normalize(['band', 2]);
+const blue = normalize(['band', 3]);
+const nir = normalize(['band', 4]);
+
+const trueColor = {
+  color: ['array', red, green, blue, 1],
+  gamma: 1.1,
+};
+
+const falseColor = {
+  color: ['array', nir, red, green, 1],
+  gamma: 1.1,
+};
+
+const ndvi = {
+  color: [
+    'interpolate',
+    ['linear'],
+    ['/', ['-', nir, red], ['+', nir, red]],
+    // color ramp for NDVI values, ranging from -1 to 1
+    -0.2,
+    [191, 191, 191],
+    -0.1,
+    [219, 219, 219],
+    0,
+    [255, 255, 224],
+    0.025,
+    [255, 250, 204],
+    0.05,
+    [237, 232, 181],
+    0.075,
+    [222, 217, 156],
+    0.1,
+    [204, 199, 130],
+    0.125,
+    [189, 184, 107],
+    0.15,
+    [176, 194, 97],
+    0.175,
+    [163, 204, 89],
+    0.2,
+    [145, 191, 82],
+    0.25,
+    [128, 179, 71],
+    0.3,
+    [112, 163, 64],
+    0.35,
+    [97, 150, 54],
+    0.4,
+    [79, 138, 46],
+    0.45,
+    [64, 125, 36],
+    0.5,
+    [48, 110, 28],
+    0.55,
+    [33, 97, 18],
+    0.6,
+    [15, 84, 10],
+    0.65,
+    [0, 69, 0],
   ],
+};
+
+const ndviPalettePlasma = {
+  color: [
+    'palette',
+    [
+      'interpolate',
+      ['linear'],
+      ['/', ['-', nir, red], ['+', nir, red]],
+      -0.2,
+      0,
+      0.65,
+      4,
+    ],
+    ['#0d0887', '#7e03a8', '#cb4778', '#f89540', '#f0f921'],
+  ],
+};
+const ndviPaletteViridis = {
+  color: [
+    'palette',
+    [
+      'interpolate',
+      ['linear'],
+      ['/', ['-', nir, red], ['+', nir, red]],
+      -0.2,
+      0,
+      0.65,
+      4,
+    ],
+    ['#440154', '#3b528b', '#21918c', '#5ec962', '#fde725'],
+  ],
+};
+
+const layer = new TileLayer({
+  style: trueColor,
+  source: new GeoTIFF({
+    normalize: false,
+    sources: [
+      {
+        url: 'https://s2downloads.eox.at/demo/EOxCloudless/2020/rgbnir/s2cloudless2020-16bits_sinlge-file_z0-4.tif',
+      },
+    ],
+  }),
 });
-source.setAttributions(
-  "<a href='https://s2maps.eu'>Sentinel-2 cloudless</a> by <a href='https://eox.at/'>EOX IT Services GmbH</a> (Contains modified Copernicus Sentinel data 2019)"
-);
-
-const ndvi = [
-  "/",
-  ["-", ["band", 2], ["band", 1]],
-  ["+", ["band", 2], ["band", 1]],
-];
-
-const ndwi = [
-  "/",
-  ["-", ["band", 3], ["band", 1]],
-  ["+", ["band", 3], ["band", 1]],
-];
 
 const map = new Map({
-  target: "map",
-  layers: [
-    new TileLayer({
-      style: {
-        color: [
-          "color",
-          // red: | NDVI - NDWI |
-          ["*", 255, ["abs", ["-", ndvi, ndwi]]],
-          // green: NDVI
-          ["*", 255, ndvi],
-          // blue: NDWI
-          ["*", 255, ndwi],
-          // alpha
-          ["band", 4],
-        ],
-      },
-      source,
-    }),
-  ],
-  view: source.getView(),
+  target: 'map',
+  layers: [layer],
+  view: new View({
+    projection: 'EPSG:4326',
+    center: [0, 0],
+    zoom: 2,
+    maxZoom: 6,
+  }),
 });
+
+const styles = {
+  trueColor,
+  falseColor,
+  ndvi,
+  ndviPalettePlasma,
+  ndviPaletteViridis,
+};
+
+const styleSelector = document.getElementById('style');
+
+function update() {
+  const style = styles[styleSelector.value];
+  layer.setStyle(style);
+}
+styleSelector.addEventListener('change', update);
